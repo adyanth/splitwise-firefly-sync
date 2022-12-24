b0835f60037b6be0262281c12129930338700cc6
Adyanth Hosavalike <me@adyanth.dev>
2022-12-23 16:48:25 -0800

    :art: Format

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from typing import Generator
 
 import os
 
+
 def getExpensesAfter(sw: Splitwise, date: datetime, user: User) -> Generator[tuple[Expense, ExpenseUser, list[str]], None, None]:
     expenses: list[Expense] = sw.getExpenses(dated_after=date.isoformat())
     for exp in expenses:
@@ -14,23 +15,25 @@ def getExpensesAfter(sw: Splitwise, date: datetime, user: User) -> Generator[tup
             continue
 
         # Get my share by userId
-        myshare: ExpenseUser = filter(lambda x: x.getId() == user.getId(), exp.getUsers()).__next__()
+        myshare: ExpenseUser = filter(
+            lambda x: x.getId() == user.getId(), exp.getUsers()).__next__()
         # Ignore transactions where I do not owe anything
         if myshare.getOwedShare() == "0.0":
             continue
 
         # Get data, latest comment > old comment > notes
         data: str = None
-        if details:=processText(exp.getDetails()):
+        if details := processText(exp.getDetails()):
             data = details
         c: Comment
         for c in sw.getComments(exp.getId()):
             if c.getCommentedUser().getId() != user.getId():
                 pass
-            if text:=processText(c.getContent()):
+            if text := processText(c.getContent()):
                 data = text
 
         yield exp, myshare, data
+
 
 def processText(text: str) -> list[str]:
     split = text.split("/")
@@ -38,7 +41,8 @@ def processText(text: str) -> list[str]:
         return split[1:]
     return []
 
-def processExp(exp: Expense, myshare:ExpenseUser, data: list[str]):
+
+def processExp(exp: Expense, myshare: ExpenseUser, data: list[str]):
     print(exp.getDescription())
     print(exp.getCurrencyCode())
     print(exp.getDate())
@@ -47,6 +51,7 @@ def processExp(exp: Expense, myshare:ExpenseUser, data: list[str]):
     print(myshare.getOwedShare())
     print(data)
     print()
+
 
 if __name__ == "__main__":
     past_day = datetime.now() - timedelta(days=1)
