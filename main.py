e0ce99bcf66dee0f585927abd8194196b0c0f452
Adyanth Hosavalike <me@adyanth.dev>
2022-12-23 16:33:33 -0800

    Add typing, get firefly categories via comments/notes

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-from splitwise import Splitwise
+from splitwise import Splitwise, Expense, User, Comment
+from splitwise.user import ExpenseUser
+from typing import Generator
 
 import os
 
-def getExpensesAfter(date, user):
-    expenses = sw.getExpenses(dated_after=date.isoformat())
+def getExpensesAfter(sw: Splitwise, date: datetime, user: User) -> Generator[tuple[Expense, ExpenseUser, list[str]], None, None]:
+    expenses: list[Expense] = sw.getExpenses(dated_after=date.isoformat())
     for exp in expenses:
         # Ignore payments
         if exp.getPayment():
             continue
 
         # Get my share by userId
-        myshare = filter(lambda x: x.getId() == user.getId(), exp.getUsers()).__next__()
+        myshare: ExpenseUser = filter(lambda x: x.getId() == user.getId(), exp.getUsers()).__next__()
         # Ignore transactions where I do not owe anything
         if myshare.getOwedShare() == "0.0":
             continue
-        
-        yield exp, myshare
 
-def printExp(exp, myshare):
+        # Get data, latest comment > old comment > notes
+        data: str = None
+        if details:=processText(exp.getDetails()):
+            data = details
+        c: Comment
+        for c in sw.getComments(exp.getId()):
+            if c.getCommentedUser().getId() != user.getId():
+                pass
+            if text:=processText(c.getContent()):
+                data = text
+
+        yield exp, myshare, data
+
+def processText(text: str) -> list[str]:
+    split = text.split("/")
+    if split[0].lower() == "firefly":
+        return split[1:]
+    return []
+
+def processExp(exp: Expense, myshare:ExpenseUser, data: list[str]):
     print(exp.getDescription())
     print(exp.getCurrencyCode())
     print(exp.getDate())
     print(exp.getCreatedAt())
-    print(exp.getCategory().getName())
+    # print(exp.getCategory().getName())
     print(myshare.getOwedShare())
+    print(data)
     print()
 
 if __name__ == "__main__":
@@ -34,5 +54,5 @@ if __name__ == "__main__":
     sw = Splitwise("", "", api_key=os.getenv("TOKEN"))
     currentUser = sw.getCurrentUser()
     print(currentUser.getFirstName())
-    for e in getExpensesAfter(past_day, currentUser):
-        printExp(*e)
+    for e in getExpensesAfter(sw, past_day, currentUser):
+        processExp(*e)
