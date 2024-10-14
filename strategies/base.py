29b2771d1b609abc1633fea8f995064586b02fac
LAPTOP-8G8AGL8G\yotam <yotamfe@gmail.com>
2024-10-14 22:13:00 +0300

    Create strategies skeleton

---
+++ strategies/base.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from splitwise import Expense
+from splitwise.user import ExpenseUser
+
+class TransactionStrategy(ABC):
+    @abstractmethod
+    def create_transactions(self, exp: Expense, myshare: ExpenseUser, data: list[str]) -> list[dict]:
+        pass
