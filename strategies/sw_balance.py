3910040b0a0cd2984600ba8d0581ac8fe9bd96cd
LAPTOP-8G8AGL8G\yotam <yotamfe@gmail.com>
2024-11-06 18:16:46 +0200

    Correctly make a deposit to the balance account
    Make a deposit to the SW balance account of amount "balance" when an expense entails people owe me money, instead of a transfer from the paying account to the SW balance, which doesn't make sense.

--- strategies/sw_balance.py
+++ strategies/sw_balance.py
@@ -15,9 +15,9 @@ class SWBalanceTransactionStrategy(TransactionStrategy):
         balance_txn = paid_txn.copy()
         balance = myshare.getNetBalance()
         if float(balance) > 0: # I payed something and people owe me money; extra money goes to balance account
-            balance_txn['source_name'] = paid_txn['source_name']
+            balance_txn['source_name'] = self._sw_balance_account + " balancer"
             balance_txn['destination_name'] = self._sw_balance_account
-            balance_txn['type'] = 'transfer'
+            balance_txn['type'] = 'deposit'
             balance_txn['description'] = f"Balance transfer for: {paid_txn['description']}"
             balance_txn = self._apply_transaction_amount(balance_txn, exp, balance)
         else: # I payed less than what I owe; I payed the remaining amount from balance account
