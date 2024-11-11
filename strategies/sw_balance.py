f0730e611337cca71466267aa33e70f7836e5ee7
LAPTOP-8G8AGL8G\yotam <yotamfe@gmail.com>
2024-11-11 23:55:53 +0200

    refactor for clarity
    explicitly handle expenses where I paid nothing but I still owe something, and the edge case where an expense was recorded but I paid my whole share.

--- strategies/sw_balance.py
+++ strategies/sw_balance.py
@@ -9,24 +9,26 @@ class SWBalanceTransactionStrategy(TransactionStrategy):
         self._apply_transaction_amount = apply_transaction_amount
 
     def create_transactions(self, exp: Expense, myshare: ExpenseUser, data: list[str]) -> list[dict]:
+        txns = {}
         paid_txn = self._get_expense_transaction_body(exp, myshare, data)
         paid_txn = self._apply_transaction_amount(paid_txn, exp, myshare.getPaidShare())
+        if float(paid_txn['amount']) != 0: # I paid; payment txn needed
+            txns['paid'] = paid_txn
 
         balance_txn = paid_txn.copy()
         balance = myshare.getNetBalance()
-        if float(balance) > 0: # I paid, I am owed; difference credited to balance account
-            balance_txn['source_name'] = self._sw_balance_account + " balancer"
-            balance_txn['destination_name'] = self._sw_balance_account
-            balance_txn['type'] = 'deposit'
-            balance_txn['description'] = f"Balance transfer for: {paid_txn['description']}"
-            balance_txn = self._apply_transaction_amount(balance_txn, exp, balance)
-        else: # I paid, I owe; difference debited from balance account
-            balance_txn['source_name'] = self._sw_balance_account
-            balance_txn['destination_name'] = paid_txn['destination_name']
-            balance_txn['type'] = "withdrawal"
-            balance_txn['description'] = f"Balance transfer for: {paid_txn['description']}"
-            balance_txn = self._apply_transaction_amount(balance_txn, exp, -float(balance))
-        txns = [paid_txn, balance_txn]
-        if float(paid_txn['amount']) == 0: # I didn't pay, I owe; debit from balance account (only one txn)
-            txns = [balance_txn]
+        if float(balance) != 0: # I owe or am owed; balance txn needed
+            txns['balance'] = balance_txn
+            if float(balance) > 0: # I am owed; difference credited to balance account
+                balance_txn['source_name'] = self._sw_balance_account + " balancer"
+                balance_txn['destination_name'] = self._sw_balance_account
+                balance_txn['type'] = 'deposit'
+                balance_txn['description'] = f"Balance transfer for: {paid_txn['description']}"
+                balance_txn = self._apply_transaction_amount(balance_txn, exp, balance)
+            else: # I owe; difference debited from balance account
+                balance_txn['source_name'] = self._sw_balance_account
+                balance_txn['destination_name'] = paid_txn['destination_name']
+                balance_txn['type'] = "withdrawal"
+                balance_txn['description'] = f"Balance transfer for: {paid_txn['description']}"
+                balance_txn = self._apply_transaction_amount(balance_txn, exp, -float(balance))
         return txns
