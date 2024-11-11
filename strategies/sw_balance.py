7d302d913861628003615e8da623d39207c1e904
LAPTOP-8G8AGL8G\yotam <yotamfe@gmail.com>
2024-11-11 21:15:26 +0200

    unify comment style

--- strategies/sw_balance.py
+++ strategies/sw_balance.py
@@ -14,19 +14,19 @@ class SWBalanceTransactionStrategy(TransactionStrategy):
 
         balance_txn = paid_txn.copy()
         balance = myshare.getNetBalance()
-        if float(balance) > 0: # I payed something and people owe me money; extra money goes to balance account
+        if float(balance) > 0: # I paid, I am owed; difference credited to balance account
             balance_txn['source_name'] = self._sw_balance_account + " balancer"
             balance_txn['destination_name'] = self._sw_balance_account
             balance_txn['type'] = 'deposit'
             balance_txn['description'] = f"Balance transfer for: {paid_txn['description']}"
             balance_txn = self._apply_transaction_amount(balance_txn, exp, balance)
-        else: # I payed less than what I owe; I payed the remaining amount from balance account
+        else: # I paid, I owe; difference debited from balance account
             balance_txn['source_name'] = self._sw_balance_account
             balance_txn['destination_name'] = paid_txn['destination_name']
             balance_txn['type'] = "withdrawal"
             balance_txn['description'] = f"Balance transfer for: {paid_txn['description']}"
             balance_txn = self._apply_transaction_amount(balance_txn, exp, -float(balance))
         txns = [paid_txn, balance_txn]
-        if float(paid_txn['amount']) == 0: # I payed nothing; only balance transaction is needed
+        if float(paid_txn['amount']) == 0: # I didn't pay, I owe; debit from balance account (only one txn)
             txns = [balance_txn]
         return txns
